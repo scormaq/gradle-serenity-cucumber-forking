ae3c79d2b65f5d81c934c3bc0604823d5bcda55c
Pavlo Vlasenko <scormaq@gmail.com>
2019-06-06 16:35:01 +0300

    Update serenity and cucumber to latest versions

--- src/test/java/com/github/scormaq/definitions/Definitions.java
+++ src/test/java/com/github/scormaq/definitions/Definitions.java
@@ -4,7 +4,7 @@ import cucumber.api.java.en.Given;
 
 public class Definitions {
 
-    @Given("there (\\d+) cucumbers? in my belly")
+    @Given("there {int} cucumber(s) in my belly")
     public void putCucumbersInBelly(Integer num) throws InterruptedException {
         Thread.sleep(2000);
         System.out.println(num + " cucumbers have been eaten");
