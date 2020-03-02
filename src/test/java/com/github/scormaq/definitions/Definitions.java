03a775cca5eb00ba420ffff662d751676560a6b6
Pavlo Vlasenko <scormaq@gmail.com>
2020-03-02 13:59:58 +0200

    Changes: * Modify gradle slicing mechanizm; * Update Serenity to 2.1.5; * Update Serenity-Cucumber4 to 2.1.2; * Update Cucumber to 4.8.0; * Update gradle wrapper to 6.2.1 * Update readme

--- src/test/java/com/github/scormaq/definitions/Definitions.java
+++ src/test/java/com/github/scormaq/definitions/Definitions.java
@@ -1,8 +1,15 @@
 package com.github.scormaq.definitions;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
+import net.serenitybdd.cucumber.suiteslicing.SerenityTags;
 
 public class Definitions {
+
+    @Before
+    public void before() {
+        SerenityTags.create().tagScenarioWithBatchingInfo();
+    }
 
     @Given("there {int} cucumber(s) in my belly")
     public void putCucumbersInBelly(Integer num) throws InterruptedException {
---
+++ src/test/java/com/github/scormaq/runners/CucumberSerneityRunner3.java
@@ -0,0 +1,10 @@
+package com.github.scormaq.runners;
+
+import org.junit.runners.model.InitializationError;
+
+public class CucumberSerneityRunner3 extends AbstractCucumberSerenityRunner {
+
+    public CucumberSerneityRunner3(Class clazz) throws InitializationError {
+        super(clazz, "3");
+    }
+}
